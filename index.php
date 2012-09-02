268a02e87a0c031c0c0fb8619a4a9d3aa61cddd7
Tomohiro, TAIRA <tomohiro.t@gmail.com>
2012-09-02 20:52:17 +0900

    add portfolio

--- index.php
+++ index.php
@@ -4,158 +4,70 @@
 <!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
 <!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
 <!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
-<head>
-  <meta charset="utf-8" />
+<!--[if gt IE 8]><!--> <html class="no-js" lang="ja"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8" />
 
-  <!-- Set the viewport width to device width for mobile -->
-  <meta name="viewport" content="width=device-width" />
+        <!-- Set the viewport width to device width for mobile -->
+        <meta name="viewport" content="width=device-width" />
 
-  <title>tomohiro.me</title>
+        <title>tomohiro.me</title>
 
-  <link rel="stylesheet" href="stylesheets/foundation.min.css">
-  <link rel="stylesheet" href="stylesheets/app.css">
+        <link rel="stylesheet" href="stylesheets/foundation.min.css">
+        <link rel="stylesheet" href="stylesheets/app.css">
 
-  <script src="javascripts/modernizr.foundation.js"></script>
+        <script src="javascripts/modernizr.foundation.js"></script>
 
-  <!-- IE Fix for HTML5 Tags -->
-  <!--[if lt IE 9]>
-    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-  <![endif]-->
+        <!-- IE Fix for HTML5 Tags -->
+        <!--[if lt IE 9]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
 
-</head>
-<body>
+    </head>
+    <body class="row">
 
-  <div class="row">
-    <div class="twelve columns">
-      <h2>Welcome to Foundation</h2>
-      <p>This is version <strong>3.0.9</strong> generated on August 15, 2012.</p>
-      <hr />
-    </div>
-  </div>
+        <header>
+            <h1>Hi, I'm Tomohiro</h1>
+            <h4 class="subheader">Developer &hearts; Shell, Ruby, JavaScript and PHP</h4>
+        </header>
 
-  <div class="row">
-    <div class="eight columns">
-      <h3>The Grid</h3>
+        <article>
+            <h2>About me</h2>
+            <section id="profile">
+            <p>I'm Tomohiro, TAIRA and a web application developer.</p>
+            </section>
+        </article>
 
-      <!-- Grid Example -->
-      <div class="row">
-        <div class="twelve columns">
-          <div class="panel">
-            <p>This is a twelve column section in a row. Each of these includes a div.panel element so you can see where the columns are - it's not required at all for the grid.</p>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="six columns">
-          <div class="panel">
-            <p>Six columns</p>
-          </div>
-        </div>
-        <div class="six columns">
-          <div class="panel">
-            <p>Six columns</p>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="four columns">
-          <div class="panel">
-            <p>Four columns</p>
-          </div>
-        </div>
-        <div class="four columns">
-          <div class="panel">
-            <p>Four columns</p>
-          </div>
-        </div>
-        <div class="four columns">
-          <div class="panel">
-            <p>Four columns</p>
-          </div>
-        </div>
-      </div>
-      
-      
-      <h3>Tabs</h3>
-      <dl class="tabs">
-        <dd class="active"><a href="#simple1">Simple Tab 1</a></dd>
-        <dd><a href="#simple2">Simple Tab 2</a></dd>
-        <dd><a href="#simple3">Simple Tab 3</a></dd>
-      </dl>
+        <article>
+            <h2>Portfolio</h2>
+            I'm create software, mostly in shell script or Ruby.
+            <div class="row">
+                <div class="twelove columns">
+                    <section id="projects">
+                        <h3>Open source software projects</h3>
+                        <ul>
+                            <li><a href="http://tomohiro.github.com/bundlizer">Bundlizer</a> | RubyGems and Bundler application manager</li>
+                            <li><a href="http://tomohiro.github.com/redmine_irc_gateway">Redmine IRC Gateway</a> | Provides an access to Redmine API via IRC Gateway</li>
+                            <li><a href="https://github.com/Tomohiro/p">p</a> | Anything distribution package management wrapper</li>
+                        </ul>
+                    </section>
 
-      <ul class="tabs-content">
-        <li class="active" id="simple1Tab">This is simple tab 1's content. Pretty neat, huh?</li>
-        <li id="simple2Tab">This is simple tab 2's content. Now you see it!</li>
-        <li id="simple3Tab">This is simple tab 3's content. It's, you know...okay.</li>
-      </ul>
-      
-      
-      
-      <h3>Buttons</h3>
+                    <section id="services">
+                        <h3>Services</h3>
+                        <ul>
+                            <li><a href="http://okinawa-movie.heroku.com">沖縄映画上映時間</a> | 沖縄県内の映画上映時間をスマートフォンから確認できるサービス</li>
+                            <li><a href="http://fluentular.heroku.com">Fluentular</a> | A Fluentd regular expression editor</li>
+                        </ul>
+                    </section>
+                    <p><a href="https://github.com/Tomohiro">and more projects...</p>
+                </div>
+            </div>
+        </article>
 
-      <div class="row">
-        <div class="six columns">
-          <p><a href="#" class="small button">Small Button</a></p>
-          <p><a href="#" class="button">Medium Button</a></p>
-          <p><a href="#" class="large button">Large Button</a></p>
-        </div>
-        <div class="six columns">
-          <p><a href="#" class="small alert button">Small Alert Button</a></p>
-          <p><a href="#" class="success button">Medium Success Button</a></p>
-          <p><a href="#" class="large secondary button">Large Secondary Button</a></p>
-        </div>
-      </div>
-      
-    </div>
+        <!-- Included JS Files (Compressed) -->
+        <script src="javascripts/foundation.min.js"></script>
 
-    <div class="four columns">
-      <h4>Getting Started</h4>
-      <p>We're stoked you want to try Foundation! To get going, this file (index.html) includes some basic styles you can modify, play around with, or totally destroy to get going.</p>
-
-      <h4>Other Resources</h4>
-      <p>Once you've exhausted the fun in this document, you should check out:</p>
-      <ul class="disc">
-        <li><a href="http://foundation.zurb.com/docs">Foundation Documentation</a><br />Everything you need to know about using the framework.</li>
-        <li><a href="http://github.com/zurb/foundation">Foundation on Github</a><br />Latest code, issue reports, feature requests and more.</li>
-        <li><a href="http://twitter.com/foundationzurb">@foundationzurb</a><br />Ping us on Twitter if you have questions. If you build something with this we'd love to see it (and send you a totally boss sticker).</li>
-      </ul>
-    </div>
-  </div>
-
-
-  <div class="row">
-    <div class="twelve columns">
-      <h3>Orbit</h3>
-      <div id="featured">
-        <img src="http://placehold.it/1200x250&text=Slide_1" alt="slide image">
-        <img src="http://placehold.it/1200x250&text=Slide_2" alt="slide image">
-        <img src="http://placehold.it/1200x250&text=Slide_3" alt="slide image">
-      </div>
-    </div>
-  </div>
-
-  <div class="row">
-    <div class="twelve columns">
-      <h3>Reveal</h3>
-      <p><a href="#" data-reveal-id="exampleModal" class="button">Example modal</a></p>
-    </div>
-  </div>
-
-  <div id="exampleModal" class="reveal-modal">
-    <h2>This is a modal.</h2>
-    <p>
-      Reveal makes these very easy to summon and dismiss. The close button is simple an anchor with a unicode 
-      character icon and a class of <code>close-reveal-modal</code>. Clicking anywhere outside the modal will 
-      also dismiss it.
-    </p>
-    <a class="close-reveal-modal">×</a>
-  </div>
-
-  <!-- Included JS Files (Compressed) -->
-  <script src="javascripts/foundation.min.js"></script>
-
-  <!-- Initialize JS Plugins -->
-  <script src="javascripts/app.js"></script>
-</body>
+        <!-- Initialize JS Plugins -->
+        <script src="javascripts/app.js"></script>
+    </body>
 </html>
